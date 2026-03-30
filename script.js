144e1f2459c3d7df34253ecbb5af0ada53c5e15d
tranphuocvinh205-bit <tranphuocvinh205@gmail.com>
2026-03-30 13:52:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Nguyễn Văn A đã  load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
